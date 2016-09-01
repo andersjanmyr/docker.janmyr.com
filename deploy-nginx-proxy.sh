1658202e05ab0c77b0b94eb759d3ca94775c8b25
Anders Janmyr <anders@janmyr.com>
2016-09-01 08:54:30 -0700

    Set pwd_dir and make docker.sock read only

--- deploy-nginx-proxy.sh
+++ deploy-nginx-proxy.sh
@@ -1,6 +1,7 @@
 
 ./update-basic-auth.sh
 
+pwd_dir='/home/ec2-user/htpasswd'
 host=docker.janmyr.com
 project=nginx-proxy
 
@@ -9,6 +10,6 @@ ssh $host "\
   docker rm $project; \
   docker run -d -p 80:80 \
   -v $pwd_dir:/etc/nginx/htpasswd \
-  -v /var/run/docker.sock:/tmp/docker.sock \
+  -v /var/run/docker.sock:/tmp/docker.sock:ro \
   --name $project \
   jwilder/nginx-proxy"
